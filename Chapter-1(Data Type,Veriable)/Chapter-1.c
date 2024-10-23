106272575888d68ce32c257f82288ef52ba94741
Moriom Nela <moriomislamnela23@gmail.com>
2024-10-23 11:59:39 +0600

    Data Type, Veriable

---
+++ Chapter-1(Data Type,Veriable)/Chapter-1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+ /*int main(){
+    int number = 25;
+    char star= '*';
+    int age = 22;
+    float pi = 3.14;
+
+    int a = 30;
+    int A = 40;
+
+ }*/
+//Sum of 2 numbers
+/*int main(){
+  int a;
+  int b;
+  printf("Enter a:");
+  scanf("%d",&a);
+  printf("Enter b:");
+  scanf("%d",&b);
+
+  printf("The sum is:%d",a+b);
+}*/
+/*int main(){
+   int side;
+   printf("Enter side:");
+   scanf("%d",&side);
+   printf("Area of Square:%d",side*side);
+}*/
+int main(){
+   float radius;
+   printf("Enter number:");
+   scanf("%f",&radius);
+   printf("Area of circle:%f",3.14*radius*radius);
+}
+
+
+
