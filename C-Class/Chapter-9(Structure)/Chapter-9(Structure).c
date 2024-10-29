9f873cb899d1b7c8572871e8f66451a0bbe563fb
Moriom Nela <moriomislamnela23@gmail.com>
2024-10-29 10:01:08 +0600

    Structure

--- C-Class/Chapter-9(Structure)/Chapter-9(Structure).c
+++ C-Class/Chapter-9(Structure)/Chapter-9(Structure).c
@@ -3,15 +3,16 @@
 
 struct student{
    int roll;
-   int cgpa;
+   float cgpa;
    char name[100];
 };
 int main(){
    struct student s1;
    s1.roll=02;
-   s1.cgpa=9.2;
+   s1.cgpa=9.5;
    strcpy(s1.name, "Nela");
    printf("Student name = %s\n",s1.name);
-   printf("Student roll = %d",s1.roll);
+   printf("Student roll = %d\n",s1.roll);
+   printf("Student CGPA = %f\n",s1.cgpa);
 
 }
